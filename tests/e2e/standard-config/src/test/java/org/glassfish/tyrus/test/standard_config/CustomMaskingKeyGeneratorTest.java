4424ea50e7b71b87796d897a9108364dc2873418
Petr Janouch <petr.janouch@oracle.com>
2014-09-12 13:35:08 +0200

    TYRUS-371: custom masking key test changed to check that the generated masking key is used in the frame

--- tests/e2e/standard-config/src/test/java/org/glassfish/tyrus/test/standard_config/CustomMaskingKeyGeneratorTest.java
+++ tests/e2e/standard-config/src/test/java/org/glassfish/tyrus/test/standard_config/CustomMaskingKeyGeneratorTest.java
@@ -40,24 +40,32 @@
 
 package org.glassfish.tyrus.test.standard_config;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.websocket.ClientEndpointConfig;
 import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
+import javax.websocket.Extension;
+import javax.websocket.MessageHandler;
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import javax.websocket.server.ServerEndpointConfig;
 
 import org.glassfish.tyrus.client.ClientManager;
 import org.glassfish.tyrus.client.ClientProperties;
 import org.glassfish.tyrus.core.MaskingKeyGenerator;
+import org.glassfish.tyrus.core.extension.ExtendedExtension;
+import org.glassfish.tyrus.core.frame.Frame;
 import org.glassfish.tyrus.server.Server;
 import org.glassfish.tyrus.test.tools.TestContainer;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -68,6 +76,8 @@ import static org.junit.Assert.fail;
  */
 public class CustomMaskingKeyGeneratorTest extends TestContainer {
 
+    private static final int MASKING_KEY = 123456;
+
     @Test
     public void testCustomMaskingKeyGenerator() {
         Server server = null;
@@ -75,29 +85,43 @@ public class CustomMaskingKeyGeneratorTest extends TestContainer {
             server = startServer(AnnotatedServerEndpoint.class);
 
             final int messageCount = 3;
-            final CountDownLatch generatorLatch = new CountDownLatch(messageCount);
+            final CountDownLatch messageLatch = new CountDownLatch(messageCount);
+            final AtomicInteger generatedMaskingKeyCounter = new AtomicInteger(0);
 
             ClientManager client = createClient();
             client.getProperties().put(ClientProperties.MASKING_KEY_GENERATOR, new MaskingKeyGenerator() {
 
-                private final Random random = new Random();
-
                 @Override
                 public int nextInt() {
-                    generatorLatch.countDown();
-                    return random.nextInt();
+                    generatedMaskingKeyCounter.incrementAndGet();
+                    return MASKING_KEY;
                 }
             });
+
+            AtomicInteger errorCounter = new AtomicInteger(0);
+
+            List<Extension> extensions = new ArrayList<Extension>();
+            extensions.add(new MaskingKeyCheckingExtension(true, errorCounter));
+
             client.connectToServer(new Endpoint() {
                 @Override
                 public void onOpen(Session session, EndpointConfig config) {
+                    session.addMessageHandler(new MessageHandler.Whole<String>() {
+                        @Override
+                        public void onMessage(String message) {
+                            messageLatch.countDown();
+                        }
+                    });
+
                     for (int i = 0; i < messageCount; i++) {
                         session.getAsyncRemote().sendText("hi");
                     }
                 }
-            }, ClientEndpointConfig.Builder.create().build(), getURI(AnnotatedServerEndpoint.class));
+            }, ClientEndpointConfig.Builder.create().extensions(extensions).build(), getURI(AnnotatedServerEndpoint.class));
 
-            assertTrue(generatorLatch.await(5, TimeUnit.SECONDS));
+            assertTrue(messageLatch.await(5, TimeUnit.SECONDS));
+            assertEquals(0, errorCounter.get());
+            assertEquals(messageCount, generatedMaskingKeyCounter.get());
         } catch (Exception e) {
             e.printStackTrace();
             fail();
@@ -106,12 +130,73 @@ public class CustomMaskingKeyGeneratorTest extends TestContainer {
         }
     }
 
-    @ServerEndpoint("/customMaskingKeyTest")
+    private static class MaskingKeyCheckingExtension implements ExtendedExtension {
+
+        private final boolean client;
+        private final AtomicInteger errorCounter;
+
+        MaskingKeyCheckingExtension(boolean client, AtomicInteger errorCounter) {
+            this.client = client;
+            this.errorCounter = errorCounter;
+        }
+
+        @Override
+        public Frame processIncoming(ExtensionContext context, Frame frame) {
+            return frame;
+        }
+
+        @Override
+        public Frame processOutgoing(ExtensionContext context, Frame frame) {
+            if (client && MASKING_KEY != frame.getMaskingKey()) {
+                // exception thrown by JUnit assert would be swallowed by extension error handling mechanism
+                errorCounter.incrementAndGet();
+            }
+
+            return frame;
+        }
+
+        @Override
+        public List<Parameter> onExtensionNegotiation(ExtensionContext context, List<Parameter> requestedParameters) {
+            return null;
+        }
+
+        @Override
+        public void onHandshakeResponse(ExtensionContext context, List<Parameter> responseParameters) {
+
+        }
+
+        @Override
+        public void destroy(ExtensionContext context) {
+
+        }
+
+        @Override
+        public String getName() {
+            return "MaskingKeyCheckingExtension";
+        }
+
+        @Override
+        public List<Parameter> getParameters() {
+            return null;
+        }
+    }
+
+    @ServerEndpoint(value = "/customMaskingKeyTest", configurator = ServerConfig.class)
     public static class AnnotatedServerEndpoint {
 
         @OnMessage
-        public void onMessage(String message) {
+        public String onMessage(String message) {
+            return message;
+        }
+    }
 
+    public static class ServerConfig extends ServerEndpointConfig.Configurator {
+
+        @Override
+        public List<Extension> getNegotiatedExtensions(List<Extension> installed, List<Extension> requested) {
+            List<Extension> extensions = new ArrayList<Extension>();
+            extensions.add(new MaskingKeyCheckingExtension(false, null));
+            return extensions;
         }
     }
 }
